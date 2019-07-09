0bd536336c16bfe516868e0950bb99247c7f7265
Morris-Chen <akari0624@hotmail.com>
2019-07-09 23:12:18 +0800

    createGlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,21 @@ import React from 'react'
 import MainPage from './pages/mainpage'
 import { render } from 'react-dom'
 
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+`
+
 const rootElement = document.getElementById('root')
-render(<MainPage />, rootElement)
+render(
+  <>
+    <GlobalStyle />
+    <MainPage />
+  </>,
+  rootElement,
+)
---
+++ src/pages/mainpage/styled.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+
+
