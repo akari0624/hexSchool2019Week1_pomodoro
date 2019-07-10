642f86c94d1a1d3a9c611e079797da08dbcdeac8
akari0624 <akari0624@hotmail.com>
2019-07-10 17:36:53 +0800

    createGlobalStyle 放在獨立的檔案中

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,17 @@
 import React from 'react'
 import MainPage from './pages/mainpage'
 import { render } from 'react-dom'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import {Provider} from 'react-redux'
 import { Themes } from './themes'
 import {appStore} from './store'
+import { StyledComponentGlobalStyle } from './themes/globalStyle'
 
-const GlobalStyle = createGlobalStyle`
-
-  html, body {
-    width: 100vw;
-    height: 100vh;
-  }
-
-  * {
-    margin: 0px;
-    padding: 0px;
-    box-sizing: border-box;
-  }
-`
 
 const rootElement = document.getElementById('root')
 render(
   <>
-    <GlobalStyle />
+    <StyledComponentGlobalStyle />
     <Provider store={appStore}>
     <ThemeProvider theme={Themes}>
       <MainPage />
