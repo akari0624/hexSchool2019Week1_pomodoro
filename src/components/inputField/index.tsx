c5d2315b1a92434af2c48873d2c22eff5faab627
akari0624 <akari0624@hotmail.com>
2019-07-11 11:27:07 +0800

    調整style設定

--- src/components/inputField/index.tsx
+++ src/components/inputField/index.tsx
@@ -1,14 +1,21 @@
-import React, { useState, useCallback } from 'react'
-import InputFieldComponent from './styled'
+import React from 'react'
+import { InputFieldComponent, InputFieldOuttestWrapper, ConfirmButtonComponent } from './styled'
 
 type EventHandler = {
   onChange: (evt: React.ChangeEvent<HTMLInputElement>) => void
+  onClick: (evt: React.MouseEvent<HTMLElement>) => void
 }
 
 const InputField: React.FunctionComponent<
   Partial<HTMLInputElement & EventHandler>
 > = props => {
-  return <InputFieldComponent {...props} />
+  return (
+ <InputFieldOuttestWrapper>
+   <InputFieldComponent {...props} />
+   <ConfirmButtonComponent onClick={props.onClick}>{" "}+{" "}</ConfirmButtonComponent>
+ </InputFieldOuttestWrapper>
+
+  )
 }
 
 export { InputField as default }
--- src/components/inputField/styled.tsx
+++ src/components/inputField/styled.tsx
@@ -1,40 +1,68 @@
 import React from 'react'
 import Styled, { ThemedStyledInterface } from 'styled-components'
 
-const InputField = Styled.input`
-  width: 100%
-  padding: 19px 16px 18px 16px;
-  color: ${props => (props.theme.inputTextColor)};
-  text-align: left;
-  font-size: 16px;
-  font-famili: Roboto, Bold Italic
-  line-height: 19px;
-  ::placeholder {color: ${props => props.theme.inputTextColor};}
-`
+const OuttestHeight = '56px'
 
-const InputFieldWrapper = Styled.div`
-  width: 445px
-  height: 19px;
-  @media (max-width:${props => props.theme.mobileOneColumn}){
+const InputFieldOuttestWrapper = Styled.div`
+  width: 445px;
+  height: ${OuttestHeight};
+  display: flex;
+  background-color: #FFFFFF;
+    @media (max-width:${props => props.theme.mobileOneColumn}){
     width: 80%;
     margin: 10px auto;
   }
 `
 
-const InputFieldOuttestWrapper = Styled.div`
-  width: 445px;
-  heightl 56px;
-  background-color: #FFFFFF;
+const InputFieldWrapper = Styled.div`
+  width: 445px
+  height: ${OuttestHeight};
+`
+
+const InputField = Styled.input`
+  width: 100%;
+  padding: 19px 16px 18px 16px;
+  color: ${props => props.theme.inputTextColor};
+  text-align: left;
+  font-size: 16px;
+  font-family: 'Roboto', Bold Italic;
+  line-height: 19px;
+  border: none; 
+  border-width: 0; 
+  box-shadow: none;
+  ::placeholder {color: ${props => props.theme.inputTextColor};}
+`
+
+const confirmButtonLength = '24px'
+
+const ConfirmButtonWrapper = Styled.div`
+  width: ${confirmButtonLength};
+  height: 24px;
+  margin: calc( (${OuttestHeight} - ${confirmButtonLength}) / 2) 16px;
+  text-align: center;
+  cursor: pointer;
+`
+
+const ConfirmButton = Styled.a`
+   text-decoration: none;
+   line-height: ${confirmButtonLength};
+   font-size: 24px;
+   font-family: 'Roboto', Bold Italic;
+   color: ${props => props.theme.inputTextColor};
 `
 
 const InputFieldComponent = (props: any) => (
-  <>
-    <InputFieldOuttestWrapper>
-      <InputFieldWrapper>
-        <InputField {...props} />
-      </InputFieldWrapper>
-    </InputFieldOuttestWrapper>
-  </>
+  <InputFieldWrapper>
+    <InputField {...props} />
+  </InputFieldWrapper>
 )
 
-export { InputFieldComponent as default }
+const ConfirmButtonComponent = (props: any) => (
+  <ConfirmButtonWrapper>
+    <ConfirmButton onClick={props.onClick}>
+      {props.children}
+    </ConfirmButton>
+  </ConfirmButtonWrapper>
+)
+
+export { InputFieldComponent, InputFieldOuttestWrapper, ConfirmButtonComponent }
