f2daf4f7f119344d65630c5eb6f52c4fb66c87ff
Morris-Chen <akari0624@hotmail.com>
2019-07-12 00:31:55 +0800

    Updates

---
+++ precache-manifest.4e1ad2f43ba506880f976f76fde5292b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ede8282a7a8cc6a7154",
+    "url": "/hexSchool2019Week1_pomodoro/static/js/runtime~main.4ede8282.js"
+  },
+  {
+    "revision": "759816b4b2b5b8d13bf2",
+    "url": "/hexSchool2019Week1_pomodoro/static/js/main.759816b4.chunk.js"
+  },
+  {
+    "revision": "ac41713a719e956c462d",
+    "url": "/hexSchool2019Week1_pomodoro/static/js/1.ac41713a.chunk.js"
+  },
+  {
+    "revision": "d9404f13af7b113db11c5227563d0249",
+    "url": "/hexSchool2019Week1_pomodoro/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hexSchool2019Week1_pomodoro/precache-manifest.8023b3f4c66af68912339253c7b73a18.js"
+  "/hexSchool2019Week1_pomodoro/precache-manifest.4e1ad2f43ba506880f976f76fde5292b.js"
 );
 
 workbox.clientsClaim();
