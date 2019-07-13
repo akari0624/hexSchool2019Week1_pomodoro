ed513758715ee3e7bcd8e54cb87a6de56c4f2721
Morris-Chen <akari0624@hotmail.com>
2019-07-13 18:27:44 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hexSchool2019Week1_pomodoro/precache-manifest.f5b5551a7d158b610824a6ef18519f11.js"
+  "/hexSchool2019Week1_pomodoro/precache-manifest.916741069eab71f67149f5746d2a1a81.js"
 );
 
 workbox.clientsClaim();
